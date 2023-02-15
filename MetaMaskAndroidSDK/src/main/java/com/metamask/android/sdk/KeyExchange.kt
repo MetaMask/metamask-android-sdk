a49da11eed69c7fc6b49eed3d699aeed2c5f3321
Mpendulo Ndlovu <mpendulo@elefantel.com>
2023-02-15 16:40:14 +0200

    Add Crypto interface and integrate in key exchange

--- MetaMaskAndroidSDK/src/main/java/com/metamask/android/sdk/KeyExchange.kt
+++ MetaMaskAndroidSDK/src/main/java/com/metamask/android/sdk/KeyExchange.kt
@@ -1,17 +1,21 @@
 package com.metamask.android.sdk
 
 import android.accessibilityservice.GestureDescription.StrokeDescription
-import android.os.Bundle
 import android.os.Message
 import android.util.Log
+import java.io.IOException
 
 data class MessageInfo (
-    val step: Int,
     val key: String?,
-    val value: String?
-)
+    val value: String?,
+    )
 
-class KeyExchange {
+data class KeyExchangeMessage(
+    val step: String,
+    val publicKey: String?
+    )
+
+class KeyExchange(crypto: Crypto) {
     companion object {
         const val TAG = "MM_ANDROID_SDK"
 
@@ -28,17 +32,39 @@ class KeyExchange {
         const val KEY_EXCHANGE_ACK = "key_exchange_ack"
     }
 
-    private val privateKey: String? = null
-    public var publicKey: String? = null
-    public var theirPublickKey: String? = null
+    private val privateKey: String?
+    private val publicKey: String?
+    private var theirPublickKey: String? = null
 
-    fun nextKeyExchangeMessage(current: String?): MessageInfo {
-        return when(current) {
-            KEY_EXCHANGE_START -> MessageInfo(KEY_EXCHANGE_STEP, KEY_EXCHANGE, KEY_EXCHANGE_SYN)
-            KEY_EXCHANGE_SYN -> MessageInfo(KEY_EXCHANGE_STEP, KEY_EXCHANGE, KEY_EXCHANGE_SYNACK)
-            KEY_EXCHANGE_SYNACK -> MessageInfo(KEY_EXCHANGE_STEP, KEY_EXCHANGE, KEY_EXCHANGE_ACK)
-            KEY_EXCHANGE_ACK -> MessageInfo(MESSAGE_STEP, null, null)
-            else -> MessageInfo(KEY_EXCHANGE_STEP, KEY_EXCHANGE, KEY_EXCHANGE_SYN)
+    private val encryption: Crypto
+
+    init {
+        encryption = crypto
+        privateKey = encryption.generatePrivateKey()
+        publicKey = encryption.publicKey(privateKey)
+    }
+
+    fun encrypt(message: String): String {
+        val key: String = theirPublickKey ?: throw NullPointerException("theirPublickKey is null")
+        return encryption.encrypt(message, key)
+    }
+
+    fun decrypt(message: String): String {
+        val key: String = privateKey ?: throw NullPointerException("privateKey is null")
+        return encryption.decrypt(message, key)
+    }
+
+    fun updateTheirPublicKey(key: String) {
+        theirPublickKey = key
+    }
+
+    fun nextKeyExchangeMessage(current: KeyExchangeMessage): MessageInfo? {
+        return when(current.step) {
+            KEY_EXCHANGE_START -> MessageInfo(KEY_EXCHANGE, KEY_EXCHANGE_SYN)
+            KEY_EXCHANGE_SYN -> MessageInfo(KEY_EXCHANGE, KEY_EXCHANGE_SYNACK)
+            KEY_EXCHANGE_SYNACK -> MessageInfo(KEY_EXCHANGE, KEY_EXCHANGE_ACK)
+            KEY_EXCHANGE_ACK -> null
+            else -> null
         }
     }
 }
