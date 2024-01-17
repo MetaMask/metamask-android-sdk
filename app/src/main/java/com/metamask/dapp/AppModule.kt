566e29c42cefefce3839d64f5fd2802ee1ba9f2f
Mpendulo Ndlovu <mpendulo@elefantel.com>
2024-01-17 09:33:57 +0200

    Update comments

--- app/src/main/java/com/metamask/dapp/AppModule.kt
+++ app/src/main/java/com/metamask/dapp/AppModule.kt
@@ -20,7 +20,7 @@ internal object AppModule {
         return DappMetadata("Droiddapp", "https://droiddapp.io", iconUrl = "https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png")
     }
 
-    @Provides
+    @Provides // Add SDKOptions(infuraAPIKey="supply_your_key_here") to Ethereum constructor for read-only calls
     fun provideEthereum(@ApplicationContext context: Context, dappMetadata: DappMetadata): Ethereum {
         return Ethereum(context, dappMetadata)
     }
